98195cc7b972809031658fcedbf5db3d70b416e7
Lufy4422 <49024246+Lufy4422@users.noreply.github.com>
2019-11-18 15:23:56 +0800

    添加主页头部、底部导航栏，中部搜索栏

--- distinctrip-vue/distinctrip/src/App.vue
+++ distinctrip-vue/distinctrip/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
+    <index-header></index-header>
+    <!-- <index-banner></index-banner> -->
+    <index-searchbar></index-searchbar><br><br><br>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IndexBottom></IndexBottom>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import IndexBottom from './components/IndexBottom.vue'
+import IndexHeader from './components/IndexHeader.vue'
+import IndexSearchbar from './components/IndexSearchbar.vue'
+// import IndexBanner from './components/IndexBanner.vue'
 
 export default {
   name: 'app',
@@ -18,11 +26,13 @@ export default {
 
 <style>
 #app {
+  padding: 0;
+  border: 0;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
