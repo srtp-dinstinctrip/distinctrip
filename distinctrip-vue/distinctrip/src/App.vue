075d605cd79d4bffc39f3a3a6be1a5d789be58e2
Lufy4422 <49024246+Lufy4422@users.noreply.github.com>
2019-11-19 10:53:12 +0800

    添加主页轮播图

--- distinctrip-vue/distinctrip/src/App.vue
+++ distinctrip-vue/distinctrip/src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app">
     <index-header></index-header>
-    <!-- <index-banner></index-banner> -->
+    <index-banner></index-banner>
     <index-searchbar></index-searchbar><br><br><br>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <IndexBottom></IndexBottom>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import IndexBottom from './components/IndexBottom.vue'
 import IndexHeader from './components/IndexHeader.vue'
 import IndexSearchbar from './components/IndexSearchbar.vue'
-// import IndexBanner from './components/IndexBanner.vue'
+import IndexBanner from './components/IndexBanner.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    IndexBottom,
+    IndexHeader,
+    IndexSearchbar,
+    IndexBanner
   }
 }
 </script>
---
+++ distinctrip-vue/distinctrip/src/components/IndexBanner.vue
@@ -0,0 +1,60 @@
+<template>
+<div id="box">
+  <el-carousel indicator-position="inside" height="500px">
+    <el-carousel-item >
+      <img src="../assets/image/img1.jpg" alt="">
+    </el-carousel-item>
+    <el-carousel-item >
+      <img src="../assets/image/img2.jpg" alt="">
+    </el-carousel-item>
+    <el-carousel-item >
+      <img src="../assets/image/img3.jpg" alt="">
+    </el-carousel-item>
+    <el-carousel-item >
+      <img src="../assets/image/img4.jpg" alt="">
+    </el-carousel-item>
+  </el-carousel>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'IndexBanner',
+    props:{
+
+    }
+}
+</script>
+
+<style scoped>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+  #box{
+    width: 1300px;
+    height: 500px;
+    position: relative;
+    left: 7.1%;
+    overflow: hidden;  /*  解除外边距塌陷  清除浮动  溢出隐藏 */
+    border-radius: 10px;
+    padding-bottom: 20px;
+  } 
+  img{
+    width: 1300px;
+    height: 500px;
+    vertical-align: top;
+    border-radius: 10px;
+  }
+</style>
