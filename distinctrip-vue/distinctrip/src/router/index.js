27ec0b5807e4729735daebafbcad2ea851108406
Angelo <artisignginree@hotmail.com>
2020-02-01 10:24:35 +0800

    完善页面路由

--- distinctrip-vue/distinctrip/src/router/index.js
+++ distinctrip-vue/distinctrip/src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
+import sign from '@/components/sign'
 import scenic from '@/components/scenic'
+import book from '@/components/book'
+import dis from '@/components/dis'
+import home from '@/components/home'
 
-import testLYJ from '@/components/text/testLYJ'
-import testWH from '@/components/text/testWH'
-import testZN from '@/components/text/testZN'
 import error from '@/components/error'
+
+import testLYJ from '@/components/test/testLYJ'
+import testWH from '@/components/test/testWH'
+import testZN from '@/components/test/testZN'
 
 Vue.use(Router)
 
@@ -17,8 +22,24 @@ export default new Router({
       component: index
     },
     {
+      path: '/sign',
+      component: sign
+    },
+    {
       path: '/scenic',
       component: scenic
+    },
+    {
+      path: '/book',
+      component: book
+    },
+    {
+      path: '/dis',
+      component: dis
+    },
+    {
+      path: '/home',
+      component: home
     },
     {
       path: '/test/zn',
